3ab3dcaf6736cf4e9618b595ea14d03d104efdfe
Claudio9905 <cloudpost10@gmail.com>
2026-06-22 15:48:45 +0200

    Add basic Footer layout and links
    Replace empty Footer with a react-bootstrap layout (Container/Row/Col). Adds placeholder logo, business P.IVA and contact headings, plus Privacy Policy and Terms & Conditions external links (open in new tab). Image src remains a placeholder.

--- sito-biglietto/src/components/Footer.jsx
+++ sito-biglietto/src/components/Footer.jsx
@@ -1,5 +1,35 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <div>
+              <img src="" alt="Logo del negozio" className="img-fluid" />
+            </div>
+          </Col>
+          <Col xs={6}>
+            <h4>P. IVA: 01234567890</h4>
+            <h4>E-mail</h4>
+            <a
+              href="https://www.gioielliaeg.com/privacy-policy-ue/?_gl=1*71kuwi*_ga*MTkxODQ4MDM1MS4xNzgxNTQ1MjM1*_ga_GW90WWK738*czE3ODIxMzU4MzkkbzQkZzAkdDE3ODIxMzU4NDMkajU2JGwwJGgw"
+              target="_blank"
+            >
+              Privacy Policy
+            </a>
+            <a
+              href="https://www.gioielliaeg.com/termini-e-condizioni/?_gl=1*71kuwi*_ga*MTkxODQ4MDM1MS4xNzgxNTQ1MjM1*_ga_GW90WWK738*czE3ODIxMzU4MzkkbzQkZzAkdDE3ODIxMzU4NDMkajU2JGwwJGgw"
+              target="_blank"
+            >
+              Termini e Condizioni
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Footer;
