54b715842daef0ef2eccf6c521326a0b332ea1ab
Claudio9905 <cloudpost10@gmail.com>
2026-06-24 14:05:11 +0200

    Add footer logo, update Footer and styles
    Import a new Logo-AeG.png and update Footer.jsx to display the site logo with centered layout and revised contact info (P. IVA and email). Add #container_footer styles to App.css for footer layout and typography. Remove unused assets: hero.png, react.svg, and vite.svg.

--- sito-biglietto/src/components/Footer.jsx
+++ sito-biglietto/src/components/Footer.jsx
@@ -1,18 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
+import LogoSito from "../assets/Logo-AeG.png";
 
 const Footer = () => {
   return (
     <>
-      <Container>
+      <Container id="container_footer">
         <Row>
-          <Col xs={6}>
-            <div>
-              <img src="" alt="Logo del negozio" className="img-fluid" />
+          <Col
+            xs={6}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <div
+              id="div_logo_footer"
+              className="d-flex justify-content-center align-items-center"
+            >
+              <img
+                src={LogoSito}
+                alt="Logo del negozio"
+                className="img-fluid w-50"
+              />
             </div>
           </Col>
           <Col xs={6}>
-            <h4>P. IVA: 01234567890</h4>
-            <h4>E-mail</h4>
+            <h4>P. IVA IT07187881219</h4>
+            <h4>info@gioielliaeg.com</h4>
             <a
               href="https://www.gioielliaeg.com/privacy-policy-ue/?_gl=1*71kuwi*_ga*MTkxODQ4MDM1MS4xNzgxNTQ1MjM1*_ga_GW90WWK738*czE3ODIxMzU4MzkkbzQkZzAkdDE3ODIxMzU4NDMkajU2JGwwJGgw"
               target="_blank"
